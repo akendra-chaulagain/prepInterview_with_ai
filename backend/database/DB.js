d81c8589f95bc4ca9bf73e608a823c1c7847715d
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2025-05-13 10:02:13 -0400

    first commit

---
+++ backend/database/DB.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(`${process.env.DATABASE_URL}/ai-interview`);
+    console.log("Database Connetcetd");
+  } catch (error) {
+    console.log("Database is not connetcted", error);
+  }
+};
+export default connectDb;
---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+
+
+import cors from "cors";
+
+// database connection
+import connectDb from "./database/DB.js";
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`server is running at port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("connection failed", err);
+  });
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    // methods: ["GET", "POST", "PUT", "DELETE"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
+    // exposedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Allow cookies
+  })
+);
+app.use(express.json());
