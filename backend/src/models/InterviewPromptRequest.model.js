7a7736730536d4dffdb12ab7c6c0c3a3b76ae49d
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2025-05-14 09:48:25 -0400

    error solved

--- backend/src/models/InterviewPromptRequest.model.js
+++ backend/src/models/InterviewPromptRequest.model.js
@@ -1,28 +1,37 @@
 import mongoose from "mongoose";
 
+const AnswerFeedbackSchema = new mongoose.Schema({
+  question: String,
+  answer: String,
+  feedback: String,
+  score: Number,
+});
+
 const interviewPromptRequestSchema = new mongoose.Schema({
   userId: {
     type: String,
     required: true,
   },
   topic: { type: String, required: true },
+  // feedbackData: [FeedbackSchema],
   interviewType: {
     type: String,
     enum: ["general", "behavioral", "technical"],
     default: "technical",
   },
+  difficulty: {
+    type: String,
+    enum: ["easy", "medium", "hard"],
+    default: "medium",
+  },
   jobRole: { type: String, required: true },
-  question: { type: String },
-  answer: { type: String },
-  feedback: { type: String },
-  score: { type: Number },
+  question: [String],
+  answers: [AnswerFeedbackSchema],
+  currentIndex: { type: Number, default: 0 },
+  completed: { type: Boolean, default: false },
+  overallScore: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  status: {
-    type: String,
-    enum: ["pending", "completed", "failed"],
-    default: "pending",
-  },
 });
 export const InterviewPromptRequest = mongoose.model(
   "InterviewPromptRequest",
--- backend/src/routers/interview.routes.js
+++ backend/src/routers/interview.routes.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
 const router = Router()
-import { postInterViewPromptRequest } from "../controllers/interview.js";
+import {  postInterviewPromptRequest } from "../controllers/interview.js";
 
 router.route("/start").post(
-  
   // // verify token
   // verifyJwt,
   // // access control
   // authorize("User", "Admin"),
-  postInterViewPromptRequest
+  postInterviewPromptRequest
 );
   
 
