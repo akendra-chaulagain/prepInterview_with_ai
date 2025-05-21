332952b408d3caa650de5696675249945bdaa07f
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2025-05-21 17:16:55 -0400

    Add interview result page and update mock interview session; implement data fetching and UI for interview completion

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,36 @@
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+// import "@clerk/themes/tailwind.css"; // optional but helpful
+
+const appearance = {
+  variables: {
+    colorPrimary: "#E90012",
+    fontSize: "16px",
+    borderRadius: "1px",
+    spacingUnit: "19px",
+    fontFamily: "Inter, sans-serif",
+    inputHeight: "490px",
+    
+  },
+
+};
+
+const localization = {
+  signIn: {
+    start: {
+      title: "Welcome Back",
+      subtitle: "Sign in to your account",
+    },
+  },
+  signUp: {
+    start: {
+      title: "Create Your AK Store Account",
+      subtitle: "Sign up to start shopping with us!",
+    },
+  },
+};
+
+
 
 export const metadata = {
   title: "AK Store",
@@ -13,8 +45,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider appearance={appearance} localization={localization} >
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
