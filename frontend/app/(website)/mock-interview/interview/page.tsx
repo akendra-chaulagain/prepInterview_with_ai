c6031b637974d6d53c8a58d8175a7aee0f8f23da
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2025-06-19 09:55:48 -0400

    error solve 2

--- frontend/app/(website)/mock-interview/interview/page.tsx
+++ frontend/app/(website)/mock-interview/interview/page.tsx
@@ -1,17 +1,14 @@
 
 export const dynamic = "force-dynamic";
+
 import React, { Suspense } from "react";
-
 import Loading from "@/components/website/Loading";
-
 import MockInterviewInner from "@/components/website/MockInterviewInner";
 
-const MockInterview = () => {
+export default function Page() {
   return (
     <Suspense fallback={<Loading message="Preparing your interview..." />}>
       <MockInterviewInner />
     </Suspense>
   );
-};
-
-export default MockInterview;
+}
--- frontend/components/website/MockInterviewInner.tsx
+++ frontend/components/website/MockInterviewInner.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
 import { axiosInstence } from "@/hooks/axiosInstence";
 import { useSearchParams } from "next/navigation";
