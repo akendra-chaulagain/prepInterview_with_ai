31c845d6b7fb35852c7764aa0fd0b67e1a07de80
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2025-06-19 12:04:22 -0400

    cors error solve 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,19 +17,20 @@ connectDb()
     console.log("connection failed", err);
   });
 app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://interview-copilot-eight.vercel.app",
+];
 app.use(
   cors({
-    // origin: "http://localhost:3000",
-    origin:"https://interview-copilot-eight.vercel.app/",
-    // methods: ["GET", "POST", "PUT", "DELETE"],
-    // allowedHeaders: ["Content-Type", "Authorization"],
-    // exposedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // Allow cookies
+    origin: allowedOrigins,
+
+    credentials: true,
   })
 );
 import practiceQuestionRoutes from "../src/routers/practiceQuestion.routes.js";
 import interviewRoutes from "../src/routers/interview.routes.js";
-import searchMockInterview  from "../src/routers/search.routes.js";
+import searchMockInterview from "../src/routers/search.routes.js";
 
 app.use("/api/v1/interview", interviewRoutes);
 app.use("/api/v1/practice-question", practiceQuestionRoutes);
